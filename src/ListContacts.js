ac9d65ff8567abec5a223f8c565b6cbba2c82521
Raffael Tancman <rtancman@gmail.com>
2018-07-31 21:34:31 -0300

    Add total list and leght query

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -18,6 +18,10 @@ class ListContacts extends Component {
     this.setState({ query: query.trim() })
   }
 
+  clearQuery = () =>{
+    this.updateQuery('')
+  }
+
   render() {
     const { contacts, onDeleteContact } = this.props
     const { query } = this.state
@@ -43,6 +47,12 @@ class ListContacts extends Component {
               onChange={(event) => this.updateQuery(event.target.value)}
             />
           </div>
+          {showingContacts.length !== contacts.length && (
+            <div className='showing-contacts'>
+              <span>Now showing {showingContacts.length} of {contacts.length} total</span>
+              <button onClick={this.clearQuery}>Show all</button>
+            </div>
+          )}
           <ol className='contact-list'>
             {showingContacts.map((contact) => (
               <li key={contact.id} className='contact-list-item'>
